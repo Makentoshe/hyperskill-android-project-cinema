7d53b7b7d056aa29591e7c5f7ffeb93086cb44a9
Makentoshe <mkliverout@gmail.com>
2021-06-10 19:56:38 +0300

    feat(stage5): update stage

--- cinema-room-manager/stage5/src/test/java/org/hyperskill/project/android/cinemaroommanager/Stage3UnitTest.kt
+++ cinema-room-manager/stage5/src/test/java/org/hyperskill/project/android/cinemaroommanager/Stage3UnitTest.kt
@@ -3,6 +3,7 @@ package org.hyperskill.project.android.cinemaroommanager
 import android.content.Intent
 import android.widget.GridLayout
 import org.junit.Assert.assertTrue
+import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
@@ -10,30 +11,34 @@ import org.robolectric.RobolectricTestRunner
 import org.robolectric.shadows.ShadowToast
 
 // Version 1.0
+// This tests are ignore due to stage4 replaces the toast with the dialog
 @RunWith(RobolectricTestRunner::class)
 class Stage3UnitTest {
 
     private val activityController = Robolectric.buildActivity(MainActivity::class.java)
 
     @Test
+    @Ignore("Not actual since 4 stage")
     fun testShouldCheckToast1() {
         toastCheck(90, 5f, 0, 0, 24.11f)
     }
 
     @Test
+    @Ignore("Not actual since 4 stage")
     fun testShouldCheckToast2() {
         toastCheck(34, 2.8f, 5, 3, 5.74f)
     }
 
     @Test
+    @Ignore("Not actual since 4 stage")
     fun testShouldCheckToast3() {
         toastCheck(108, 4.5f, 5, 6, 11.18f)
     }
 
     private fun toastCheck(duration: Int, rating: Float, row: Int, place: Int, price: Float) {
         val message = """
-            do you properly calculates a ticket price? 
-            For ${rating}f rating and $duration duration the ticket price 
+            do you properly calculates a ticket price?
+            For ${rating}f rating and $duration duration the ticket price
             in ${row + 1} row and in ${place + 1} place should be $price$
         """.trimMargin()
 
