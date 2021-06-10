0e96a82016ef29ea18a2bce33d1ccf3a4a20af28
Makentoshe <mkliverout@gmail.com>
2021-06-10 17:54:41 +0300

    feat(stage4): revert removed stage3 tests
    using @Ignore annotation instead default commenting

--- cinema-room-manager/stage4/src/test/java/org/hyperskill/project/android/cinemaroommanager/Stage3UnitTest.kt
+++ cinema-room-manager/stage4/src/test/java/org/hyperskill/project/android/cinemaroommanager/Stage3UnitTest.kt
@@ -1,8 +1,14 @@
 package org.hyperskill.project.android.cinemaroommanager
 
+import android.content.Intent
+import android.widget.GridLayout
+import org.junit.Assert.assertTrue
+import org.junit.Ignore
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.shadows.ShadowToast
 
 // Version 1.0
 // This tests are ignore due to stage4 replaces the toast with the dialog
@@ -11,38 +17,41 @@ class Stage3UnitTest {
 
     private val activityController = Robolectric.buildActivity(MainActivity::class.java)
 
-//    @Test
-//    fun testShouldCheckToast1() {
-//        toastCheck(90, 5f, 0, 0, 24.11f)
-//    }
-//
-//    @Test
-//    fun testShouldCheckToast2() {
-//        toastCheck(34, 2.8f, 5, 3, 5.74f)
-//    }
-//
-//    @Test
-//    fun testShouldCheckToast3() {
-//        toastCheck(108, 4.5f, 5, 6, 11.18f)
-//    }
-//
-//    private fun toastCheck(duration: Int, rating: Float, row: Int, place: Int, price: Float) {
-//        val message = """
-//            do you properly calculates a ticket price?
-//            For ${rating}f rating and $duration duration the ticket price
-//            in ${row + 1} row and in ${place + 1} place should be $price$
-//        """.trimMargin()
-//
-//        val activity = activityController.apply {
-//            get().intent = Intent().apply {
-//                putExtra("DURATION", duration)
-//                putExtra("RATING", rating)
-//            }
-//        }.setup().get()
-//
-//        val gridLayout = activity.findViewById<GridLayout>(R.id.cinema_room_places)
-//        gridLayout.getChildAt(row * gridLayout.columnCount + place).performClick()
-//
-//        assertTrue(message, ShadowToast.showedToast("$price$"))
-//    }
+    @Test
+    @Ignore("Not actual since 4 stage")
+    fun testShouldCheckToast1() {
+        toastCheck(90, 5f, 0, 0, 24.11f)
+    }
+
+    @Test
+    @Ignore("Not actual since 4 stage")
+    fun testShouldCheckToast2() {
+        toastCheck(34, 2.8f, 5, 3, 5.74f)
+    }
+
+    @Test
+    @Ignore("Not actual since 4 stage")
+    fun testShouldCheckToast3() {
+        toastCheck(108, 4.5f, 5, 6, 11.18f)
+    }
+
+    private fun toastCheck(duration: Int, rating: Float, row: Int, place: Int, price: Float) {
+        val message = """
+            do you properly calculates a ticket price?
+            For ${rating}f rating and $duration duration the ticket price
+            in ${row + 1} row and in ${place + 1} place should be $price$
+        """.trimMargin()
+
+        val activity = activityController.apply {
+            get().intent = Intent().apply {
+                putExtra("DURATION", duration)
+                putExtra("RATING", rating)
+            }
+        }.setup().get()
+
+        val gridLayout = activity.findViewById<GridLayout>(R.id.cinema_room_places)
+        gridLayout.getChildAt(row * gridLayout.columnCount + place).performClick()
+
+        assertTrue(message, ShadowToast.showedToast("$price$"))
+    }
 }
