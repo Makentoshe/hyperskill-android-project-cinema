505f2aeb87915bcc585fb20ad7dc77034741d622
Makentoshe <mkliverout@gmail.com>
2021-11-06 15:29:18 +0300

    test(stage4): fix several issues and improvements

--- Cinema Room Manager/stage4/src/test/java/org/hyperskill/cinema/AbstractUnitTest.kt
+++ Cinema Room Manager/stage4/src/test/java/org/hyperskill/cinema/AbstractUnitTest.kt
@@ -1,6 +1,8 @@
 package org.hyperskill.cinema
 
 import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context
 import android.view.View
 import org.junit.Before
 import org.robolectric.Robolectric
@@ -18,13 +20,23 @@ abstract class AbstractUnitTest<T : Activity>(private val activityClass: Class<T
         activityController = Robolectric.buildActivity(activityClass)
     }
 
+    protected fun Context.identifier(id: String, `package`: String = packageName) : Int {
+        return resources.getIdentifier(id, "id", `package`)
+    }
+
     protected fun identifier(id: String) : Int {
         return activity.resources.getIdentifier(id, "id", activity.packageName)
+    }
+
+    protected fun <T: View> View.find(id: String): T = findViewById(context.identifier(id))
+
+    protected fun <T: View> MainActivity.find(id: String) : T = findViewById(identifier(id))
+
+    protected fun <T: View> AlertDialog.find(id: String, `package`: String = context.packageName) : T {
+        return findViewById(context.identifier(id, `package`))
     }
 
     protected fun <T: View> find(id: Int) : T = activity.findViewById(id)
 
     protected fun <T: View> find(id: String) : T = activity.findViewById(identifier(id))
-
-    protected fun <T: View> View.find(id: String): T = findViewById(identifier(id))
 }
--- Cinema Room Manager/stage5/src/test/java/org/hyperskill/cinema/Stage4UnitTest.kt
+++ Cinema Room Manager/stage5/src/test/java/org/hyperskill/cinema/Stage4UnitTest.kt
@@ -1,70 +1,53 @@
 package org.hyperskill.cinema
 
+import android.app.Activity
+import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Intent
 import android.graphics.Color
 import android.os.Looper
+import android.view.View
 import android.widget.Button
 import android.widget.GridLayout
 import android.widget.TextView
 import androidx.cardview.widget.CardView
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNotEquals
+import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.Shadows
+import org.robolectric.android.controller.ActivityController
 import org.robolectric.shadows.ShadowAlertDialog
+import java.util.*
+import kotlin.math.abs
 
-// Version 17.10.2021
+// Version 6.11.2021
 @RunWith(RobolectricTestRunner::class)
-class Stage4UnitTest {
-
-    private val activityController = Robolectric.buildActivity(MainActivity::class.java)
+class Stage4UnitTest : AbstractUnitTest<MainActivity>(MainActivity::class.java) {
 
     @Test
     fun testShouldCheckDialogTitle() {
         val message = "make sure you pass a row number and a place number properly"
 
-        val activity = activityController.apply {
-            get().intent = Intent().apply {
-                putExtra("DURATION", 90)
-                putExtra("RATING", 5.0f)
-            }
-        }.setup().get()
-
-        val gridLayout = activity.findViewById<GridLayout>(R.id.cinema_room_places)
-        gridLayout.getChildAt(0 * gridLayout.columnCount + 5).performClick()
-        Shadows.shadowOf(Looper.getMainLooper()).runToEndOfTasks()
-
-        val dialog = ShadowAlertDialog.getLatestAlertDialog()
-        val titleId: Int = dialog.context.resources.getIdentifier("alertTitle", "id", "android")
-        val titleView = dialog.findViewById<TextView>(titleId)
-
-        assertEquals(message, "Buy a ticket 1 row 6 place", titleView.text)
+        activityController.`launch this activity and execute`(arguments = `most profitable movie`()) {
+            `grid layout child`(index = 5).`perform click`()
+            `in alert dialog`().`for dialog title`().`text should be`(message, "Buy a ticket 1 row 6 place")
+        }
     }
 
     @Test
     fun testShouldCheckDialogMessage() {
         val message = "make sure you pass a ticket price properly"
 
-        val activity = activityController.apply {
-            get().intent = Intent().apply {
-                putExtra("DURATION", 90)
-                putExtra("RATING", 5.0f)
+        activityController.`launch this activity and execute`(arguments = `most profitable movie`()) {
+            `grid layout child`(index = 5).`perform click`()
+            `in alert dialog`().`for dialog message`().`text should be`(message) { text ->
+                val startsWith = text.startsWith("Your ticket price is ")
+                val endsWith = text.endsWith("$")
+                val double = text.replace("$", "").`is contain double`(expected = 24.11, `with delta` = 0.1)
+                return@`text should be` startsWith and endsWith and double
             }
-        }.setup().get()
-
-        val gridLayout = activity.findViewById<GridLayout>(R.id.cinema_room_places)
-        gridLayout.getChildAt(0 * gridLayout.columnCount + 5).performClick()
-        Shadows.shadowOf(Looper.getMainLooper()).runToEndOfTasks()
-
-        val dialog = ShadowAlertDialog.getLatestAlertDialog()
-        val messageId: Int = dialog.context.resources.getIdentifier("message", "id", "android")
-        val messageView = dialog.findViewById<TextView>(messageId)
-
-        assertEquals(message, "Your ticket price is 24.11\$", messageView.text)
+        }
     }
 
     @Test
@@ -79,7 +62,7 @@ class Stage4UnitTest {
             }
         }.setup().get()
 
-        val gridLayout = activity.findViewById<GridLayout>(R.id.cinema_room_places)
+        val gridLayout = activity.find<GridLayout>("cinema_room_places")
         val gridLayoutChild = gridLayout.getChildAt(0 * gridLayout.columnCount + 5)
 
         // Click to grid element to invoke dialog
@@ -93,7 +76,7 @@ class Stage4UnitTest {
         Shadows.shadowOf(Looper.getMainLooper()).idle()
 
         // Check the grid element indicator
-        val indicator = gridLayoutChild.findViewById<CardView>(R.id.cinema_room_place_indicator)
+        val indicator = gridLayoutChild.find<CardView>("cinema_room_place_indicator")
         assertEquals(message1, indicator.cardBackgroundColor.defaultColor, Color.DKGRAY)
 
         // Click to grid element to try to invoke dialog again
@@ -103,6 +86,38 @@ class Stage4UnitTest {
         // Check dialog does not appears
         val dialog2 = ShadowAlertDialog.getLatestAlertDialog()
         assertEquals(message2, dialog1, dialog2)
+    }
+
+    @Test
+    fun testShouldCheckDialogNegativeButtonText() {
+        activityController.`launch this activity and execute` {
+            `grid layout child`(index = 5).`perform click`()
+            `in alert dialog`().`for negative button`() `text should be` "Cancel purchase"
+        }
+    }
+
+    @Test
+    fun testShouldCheckDialogNegativeButtonVisibility() {
+        activityController.`launch this activity and execute` {
+            `grid layout child`(index = 5).`perform click`()
+            `in alert dialog`().`for negative button`() `visibility should be` View.VISIBLE
+        }
+    }
+
+    @Test
+    fun testShouldCheckDialogPositiveButtonText() {
+        activityController.`launch this activity and execute` {
+            `grid layout child`(index = 5).`perform click`()
+            `in alert dialog`().`for positive button`() `text should be` "Buy a ticket"
+        }
+    }
+
+    @Test
+    fun testShouldCheckDialogPositiveButtonVisibility() {
+        activityController.`launch this activity and execute` {
+            `grid layout child`(index = 5).`perform click`()
+            `in alert dialog`().`for positive button`() `visibility should be` View.VISIBLE
+        }
     }
 
     @Test
@@ -128,7 +143,7 @@ class Stage4UnitTest {
         Shadows.shadowOf(Looper.getMainLooper()).runToEndOfTasks()
 
         // Check the grid element indicator
-        val indicator = gridLayoutChild.findViewById<CardView>(R.id.cinema_room_place_indicator)
+        val indicator = gridLayoutChild.find<CardView>("cinema_room_place_indicator")
         val message1 = """
             Make sure you do nothing, if purchase was canceled (Indicator color and Indication color should not being equal)
             Indicator color: ${indicator.cardBackgroundColor.defaultColor}
@@ -145,8 +160,70 @@ class Stage4UnitTest {
         val message2 = """
             Alert dialog should be displayed again if the previous purchase was canceled (dialogs should be different)
             First dialog: $dialog1
-            Secod dialog: $dialog2
+            Second dialog: $dialog2
         """.trimMargin()
         assertNotEquals(message2, dialog1, dialog2)
     }
+
+    private inline fun <reified A : Activity> ActivityController<A>.`launch this activity and execute`(
+        arguments: Intent? = Intent(),
+        crossinline action: A.() -> Unit
+    ): ActivityController<A> {
+        get().intent = arguments
+        return setup().also { it.get().apply(action) }
+    }
+
+    private fun MainActivity.`grid layout child`(index: Int): View {
+        return find<GridLayout>("cinema_room_places").getChildAt(index)
+    }
+
+    private fun View.`perform click`() {
+        performClick()
+        Shadows.shadowOf(Looper.getMainLooper()).runToEndOfTasks()
+    }
+
+    private fun `in alert dialog`(): AlertDialog = ShadowAlertDialog.getLatestAlertDialog()
+
+    private fun AlertDialog.`for negative button`(): Button = getButton(Dialog.BUTTON_NEGATIVE)
+
+    private fun AlertDialog.`for dialog title`(): TextView = find("alertTitle", "android")
+
+    private fun AlertDialog.`for dialog message`(): TextView = find("message", "android")
+
+    private fun AlertDialog.`for positive button`(): Button = getButton(Dialog.BUTTON_POSITIVE)
+
+    private infix fun View.`visibility should be`(visibility: Int) {
+        assertEquals(visibility, this.visibility)
+    }
+
+    private infix fun TextView.`text should be`(string: String) {
+        assertEquals("Expected a $string text in $this", string, text)
+    }
+
+    private fun TextView.`text should be`(errorMessage: String, expected: String) {
+        assertEquals(errorMessage, expected, text)
+    }
+
+    private fun TextView.`text should be`(errorMessage: String, action: (String) -> Boolean) {
+        assertTrue(errorMessage, action(text.toString()))
+    }
+
+    private fun `most profitable movie`() = Intent().apply {
+        putExtra("DURATION", 90)
+        putExtra("RATING", 5.0f)
+    }
+
+    private fun String.`is contain double`(expected: Double, `with delta`: Double): Boolean {
+        val scanner = Scanner(this).useLocale(Locale.US)
+        while (scanner.hasNext()) {
+            if (scanner.hasNextDouble()) {
+                val scanned = scanner.nextDouble()
+                if (scanned.compareTo(expected) == 0) return true
+                return abs(expected - scanned) > `with delta`
+            }
+            scanner.next()
+        }
+        return false
+    }
+
 }
